335ce3210d11596c377e2874404657690fb9aa05
RomanSukharev <119834588+RomanSukharev@users.noreply.github.com>
2024-08-19 11:49:48 +0000

    Deploying to gh-pages from @ RomanSukharev/suchkov_bureau@6a89cd7221c344b6ceaa6c113d6ff47ac9e21de9 🚀

---
+++ _nuxt/DG7qOK0q.js
@@ -0,0 +1,1 @@
+import{_ as t,v as o,a3 as s,t as a}from"./Brw9xhcr.js";const r={};function n(e,c,l,_,d,f){return a(),o("div",null,[s(e.$slots,"default")])}const u=t(r,[["render",n]]);export{u as default};
